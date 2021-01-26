298d397584a0072630b27b3cf15626f856eb4bcb
崔 明辉 <cuis@vip.qq.com>
2021-01-26 11:24:38 +0800

    fix: constrained_box empty state.

--- lib/components/constrained_box.dart
+++ lib/components/constrained_box.dart
@@ -1,16 +1,37 @@
 part of '../mpcore.dart';
 
 MPElement _encodeConstrainedBox(Element element) {
-  final widget = element.widget as ConstrainedBox;
+  // final widget = element.widget as ConstrainedBox;
   final childElements = MPElement.childrenFromFlutterElement(element);
+  BoxConstraints constraints;
+  if (childElements.isEmpty) {
+    if (element.findRenderObject() is RenderConstrainedBox) {
+      constraints = (element.findRenderObject() as RenderConstrainedBox)
+          ?.additionalConstraints;
+      if (constraints.hasInfiniteWidth && constraints.hasInfiniteHeight) {
+        constraints = BoxConstraints(
+          minWidth: 0,
+          minHeight: 0,
+          maxWidth: 0,
+          maxHeight: 0,
+        );
+      }
+    } else {
+      constraints = BoxConstraints(
+        minWidth: 0,
+        minHeight: 0,
+        maxWidth: 0,
+        maxHeight: 0,
+      );
+    }
+  } else {
+    // ignore: invalid_use_of_protected_member
+    constraints = element.findRenderObject()?.constraints;
+  }
   return MPElement(
     name: 'constrained_box',
     children: childElements,
-    // ignore: invalid_use_of_protected_member
-    constraints: childElements.isEmpty
-        ? (element.findRenderObject() as RenderConstrainedBox)
-            ?.additionalConstraints
-        : element.findRenderObject()?.constraints,
+    constraints: constraints,
     attributes: {
       'scrollable': MPCore.findTarget<Scrollable>(element) != null,
     },
